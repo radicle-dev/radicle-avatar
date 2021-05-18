b555a04c24c723678bf0c30838a17d01c65b9533
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-05-18 14:40:40 +0200

    Generate fixtures for the whole character range
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- examples/generate-fixtures.rs
+++ examples/generate-fixtures.rs
@@ -1,7 +1,7 @@
 use radicle_avatar::{Avatar, Usage};
-use rand::{distributions::Alphanumeric, Rng};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
-use std::{fs, iter};
+use std::{char, fs};
 
 #[derive(Serialize, Deserialize)]
 struct Input {
@@ -20,10 +20,9 @@ fn main() -> std::io::Result<()> {
     let mut rng = rand::thread_rng();
 
     for _ in 0..1000 {
-        let random_string: String = iter::repeat(())
-            .map(|()| rng.sample(Alphanumeric))
-            .map(char::from)
-            .take(40)
+        let random_string_size = rng.gen_range(0..=40);
+        let random_string: String = (0..random_string_size)
+            .map(|_| rng.gen_range('\u{0000}'..char::MAX))
             .collect();
 
         let input_any = Input {
